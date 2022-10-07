3b76953b62c3a36b48424c80d36ae7d769a48676
Luiizmiranda <luizmiranda564@gmail.com>
2022-10-07 16:45:12 -0400

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -2,7 +2,7 @@ var vel =50
 
 console.log(` A velocidade do seu carro é ${vel}km/h`)
 
-if(vel > 60) {//condição simples
+if(vel > 60) {//condição simples    
 
     console.log(`Você ultrapaassou a velocidade permetida.MULTADO!`)
 
