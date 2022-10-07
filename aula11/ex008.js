b88bc0bec459649909733d389679ac9dca7a4ad2
Luiizmiranda <luizmiranda564@gmail.com>
2022-10-07 16:44:28 -0400

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel =50
+
+console.log(` A velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60) {//condição simples
+
+    console.log(`Você ultrapaassou a velocidade permetida.MULTADO!`)
+
+}
+console.log(`Dirija sempre com cinto de segurança`)
