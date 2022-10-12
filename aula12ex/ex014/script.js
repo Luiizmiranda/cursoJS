fe42905ed0e44f4a51a1326b263dbb79a385269a
Luiizmiranda <luizmiranda564@gmail.com>
2022-10-12 00:35:13 -0400

    .

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert("Olá!")
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML =`Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        //  BOM DIA!
+        img.src='manha.png'
+        document.body.style.background='#ff9c00'
+    }else if( hora >=12 && hora < 18){
+        //BOA TARDE
+        img.src ='tarde.png'
+        document.body.style.background='#879693'
+    }else {
+        //BOA NOITE
+        img.src = 'noite.png'
+        document.body.style.background='#d098bb'
+
+    }
+}
