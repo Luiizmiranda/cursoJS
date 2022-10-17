f7bdf641037e45321589bd739230130c66dbede1
Luiizmiranda <luizmiranda564@gmail.com>
2022-10-17 19:34:00 -0400

    .

---
+++ aula15/ambiente.js
@@ -0,0 +1,5 @@
+let num =[5,8,2,9,3]
+num.push(1)//vai colocar o número na frente   se eu colocar primeiro num.sort vai prevalecer o comando 
+num.sort() //esse serve para colocar em ordem
+console.log(`p vetor tem ${num.length}`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,19 @@
+let valores = [8,7,4,2,9]
+
+/*for(let pos=0;pos < valores.length;  pos++ ){
+    console.log(`A posição ${pos} tem valor ${valores[pos   ]}`)
+}*/  
+
+for(let pos in valores){
+    console.log(valores[pos])
+}
+
+/* num.indexOf(7) esse comando ele vai procurar dentro da num número 7 
+
+num
+[4][5][6][7][8]
+ 0  1  2  3  4
+
+
+
+*/
