67365a98cad428405e89d509664f70c8dc4f3369
Luiizmiranda <luizmiranda564@gmail.com>
2022-10-24 17:43:21 -0400

    .

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -3,17 +3,17 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores =[]
 
-function isNumero(n){
-    if(Number(n) >=1 && Number(n) <= 100){
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100){
     return true
 } else {
     return false
 }
 }
-function inLista(n,l) {
-    if(l.indexOF(Number(n))!= -1){
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
         return true
-    }else{
+    } else {
         return false
     }
 
@@ -21,8 +21,40 @@ function inLista(n,l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value,valores))  {
-        window.alert('Tudo ok')
-    }else{
-        alert('Valor inválido ou já encontrado na lista.')
+       valores.push(Number(num.value))
+       let item = document.createElement('option')
+       item.text = `Valor ${num.value}adicionado.`
+       lista.appendChild(item)
+       res.innerHTML=''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value =''
+    num.focus()
+}
+
+function Finalizar() {
+    if(valores.length == 0) {
+    window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores) {
+        if(valores[pos] > maior)
+            maior= valores[pos]
+            if(valores[pos] < menor)
+                menor =valores[pos]
+         }    
+
+        res.innerHTML =''
+        res.innerHTML +=`<p> Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML +=`<p> O menor valor informado foi ${maior}.</p`
+        res.innerHTML +=`<p> O menor valor informado foi ${menor}.</p>`
+        res.innerHTML +=`<p> Somando todos os valores,temos${soma}.</p>`
+    }
+
 }
