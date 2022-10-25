9e9c6ac9051d24d5bd2ee1b6766f3a8be4d33896
Luiizmiranda <luizmiranda564@gmail.com>
2022-10-25 11:37:57 -0400

    Update script.js

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -23,7 +23,7 @@ function adicionar() {
     if(isNumero(num.value) && !inLista(num.value,valores))  {
        valores.push(Number(num.value))
        let item = document.createElement('option')
-       item.text = `Valor ${num.value}adicionado.`
+       item.text = `Valor ${num.value} adicionado.`
        lista.appendChild(item)
        res.innerHTML=''
     } else {
@@ -44,17 +44,19 @@ function Finalizar() {
         let media = 0
 
         for(let pos in valores) {
+            soma += valores[pos]
         if(valores[pos] > maior)
             maior= valores[pos]
             if(valores[pos] < menor)
                 menor =valores[pos]
          }    
-
+        media = soma / tot
         res.innerHTML =''
         res.innerHTML +=`<p> Ao todo, temos ${tot} números cadastrados.</p>`
         res.innerHTML +=`<p> O menor valor informado foi ${maior}.</p`
         res.innerHTML +=`<p> O menor valor informado foi ${menor}.</p>`
-        res.innerHTML +=`<p> Somando todos os valores,temos${soma}.</p>`
+        res.innerHTML +=`<p> Somando todos os valores,temos ${soma}.</p>`
+        res.innerHTML += `<p> A méddia dos valoresz digitados é ${media}<p/>`
     }
 
 }
